eaa27b55df00761e76c55a5d7d0cfd3d4c7349a6
Anton Teliga <116106070+ateliga@users.noreply.github.com>
2023-06-13 20:47:35 +0300

    update code

--- lab09/prj/main.cpp
+++ lab09/prj/main.cpp
@@ -1,11 +1,44 @@
 #include <iostream>
 #include <cmath>
 #include <windows.h>
+#include <ModulesTeliga.h>
 
 using namespace std;
-
+bool logic(double a,double b){
+    bool res = abs(a-23)<=b+5;
+    return res;
+}
 void s_calculation() {
     cout << "‘ункц≥€ s_calculation() викликана!" << endl;
+
+    double n_x, n_y, n_z, n_S;
+    double a, b;
+    int x, y, z, S;
+    cout << "¬вед≥ть x: ";
+    cin >> n_x;
+    cout << "¬вед≥ть y: ";
+    cin >> n_y;
+    cout << "¬вед≥ть z: ";
+    cin >> n_z;
+    cout << "¬вед≥ть a: ";
+    cin >> a;
+    cout << "¬вед≥ть b: ";
+    cin >> b;
+    n_S = s_calculation(n_x, n_y, n_z);
+    cout << "S = " << S << endl;
+    x= n_x;
+    y= n_y;
+    z= n_z;
+    S= n_S;
+    cout<< "–езультат лог≥чного виразу:"<<logic(a,b)<< endl;
+    cout << "x (дес€ткове) = " << x << endl;
+    cout << "y (дес€ткове) = " << y << endl;
+    cout << "z (дес€ткове) = " << z << endl;
+    cout << "S (дес€ткове) = " << S << endl;
+    cout << "x (ш≥стнадц€ткове) = " << hex << x << endl;
+    cout << "y (ш≥стнадц€ткове) = " << hex << y << endl;
+    cout << "z (ш≥стнадц€ткове) = " << hex << z << endl;
+    cout << "S (ш≥стнадц€ткове) = " << hex << S << endl;
 }
 void task_9_1() {
     float depositAmount;
